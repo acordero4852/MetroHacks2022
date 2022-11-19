1856ade6b24892432716b4decdd57da5ebfda592
Andrew Yu <andrewyu209@gmail.com>
2022-11-19 13:09:24 -0500

    Add mongoose/mongodb and set up server.js

---
+++ server/server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+// const path = require("path");
+const db = require("./config/connection");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "../client/build")));
+
+app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
