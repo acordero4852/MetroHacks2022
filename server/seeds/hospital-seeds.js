561790c7179a166f22410f039f2639ea0dcb315d
Brandon Atwal <atwalbrandon@gmail.com>
2022-11-19 16:22:50 -0600

    fixed error with hospital model

--- server/seeds/hospital-seeds.js
+++ server/seeds/hospital-seeds.js
@@ -3,9 +3,16 @@ const { Hospital } = require("../models");
 const hospitalData = [
     {
         hospitalName: "Test1",
-        phoneNumer: "(940) 777 1234",
+        phoneNumber: "(940) 777 1234",
         address: "1234 Main St",
         ZIP: "75033"
+
+    },
+    {
+        hospitalName: "Hospt2",
+        phoneNumber: "(777) 777 1234",
+        address: "555 Main St",
+        ZIP: "75026"
 
     },
 ];
--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const db = require("./config/connection");
 const seedDoctor = require("./seeds/doctor-seeds");
-
+const seedHospital = require("./seeds/hospital-seeds");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -12,5 +12,6 @@ app.use(express.static(path.join(__dirname, "../client/build")));
 
 db.once("open", () => {
   seedDoctor();
+  seedHospital();
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
 });
