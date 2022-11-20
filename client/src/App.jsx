7451dc73196cb232f62166b1f03ac20b669f6294
acordero4852 <corderoandy5@gmail.com>
2022-11-19 23:28:02 -0500

    added content to landing page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,17 @@
+import Landing from './pages/Landing';
+import Nearest from './pages/Nearest';
+import Hospital from './pages/Hospital';
 import { Routes, Route } from 'react-router-dom';
-import './App.scss'
+import './App.scss';
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Routes>
+        <Route exact path="/" element={<Landing />} />
+        <Route path="/nearest" element={<Nearest />} />
+        <Route path="/hospital" element={<Hospital />} />
+      </Routes>
     </div>
-  )
+  );
 }
